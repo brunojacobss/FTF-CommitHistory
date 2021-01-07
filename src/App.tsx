96d28b6ef716e8f96da0bfd3723c80958e1eb1f3
brunojacobs1 <brunojacobsschulz@gmail.com>
2021-01-07 01:29:05 -0500

    :lipstick: Add bootstrap CDN to start styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Jumbotron } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
 import { CommitList } from './components/commit-list';
-function App() {
-  return (
-    <div>
-      <CommitList />
-    </div>
-  );
-}
 
-export default App;
+export const App: React.FC = () => {
+  return (
+    <Container>
+      <Jumbotron>
+        <h1 className="header">Github commit history</h1>
+      </Jumbotron>
+      <CommitList />
+    </Container>
+  );
+};
